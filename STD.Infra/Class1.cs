1476dc2d85972dca2104e8f8af50a3ac5dbfb819
Gabriel Silva de Camargo <gabriel.camargo@wevo.com.br>
2019-07-05 09:13:43 -0300

    Add project files.

---
+++ STD.Infra/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace STD.Infra
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/STD.ApplicationCore/Entity/Cliente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace STD.ApplicationCore.Entity
+{
+    public class Cliente
+    {
+        public Cliente()
+        {
+
+        }
+
+        public int clienteid {get;set;}
+        public string nome { get; set; }
+        public int CPF { get; set; }
+    }
+}
